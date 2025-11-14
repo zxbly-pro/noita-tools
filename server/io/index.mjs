fa133cf2c3c014c16d730ff454b974a6bac538f8
luoju <QWE123qwe!>
2025-11-14 17:14:14 +0800

    chore(docker): 更新.dockerignore文件配置
    - 移除了server目录的忽略规则
    - 调整了.dockerignore文件的配置顺序
    -优化了

--- server/io/index.mjs
+++ server/io/index.mjs
@@ -5,7 +5,10 @@ import { handleCompute, counts } from "./compute.mjs";
 import { rooms } from "./rooms.mjs";
 
 const corsDomains = [
-  "www.zxbly.com",
+  "www.zxbly.com:3001",
+  "www.zxbly.com:300",
+  "zxbly.com:3001",
+  "zxbly.com:300",
   "localhost:3000",
   "localhost:3001",
   "127.0.0.1:3000",
